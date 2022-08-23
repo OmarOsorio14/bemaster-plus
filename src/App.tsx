a971b044d2f2d2b1141c54f9e6dc4eb3b2c339dc
Omar Hernan Osorio Porras <omarhop1114@gmail.com>
2022-08-23 11:16:46 -0500

    add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Route,Routes, Navigate} from 'react-router-dom'
+import Home from './pages/Home';
+import Login from './pages/Login'
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-			
+			<Routes>
+				<Route path="/" element={<Home/>} />
+				<Route path="/login" element={<Login/>} />
+			</Routes>
     </div>
   );
 }
---
+++ src/assets/credentials.ts
@@ -0,0 +1,1 @@
+export const email = 'prueba@gmail.com';
